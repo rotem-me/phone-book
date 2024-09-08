e26d30cd4d7c5970356e9aaa5a82ec2c6519206b
rotem-me <54479916+rotem-me@users.noreply.github.com>
2024-09-08 16:48:00 +0300

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -3,8 +3,8 @@ export enum ENVS {
   TEST = "test",
 }
 export const APP_NAME = "PHONE-BOOK";
-export const NODE_ENV = ENVS.DEV; //process.env.NODE_ENV || ENVS.DEV;
-export const PHONE_BOOK_PORT = 7010; //process.env.PHONE_BOOK_PORT || 7010;
+export const NODE_ENV = ENVS.DEV;
+export const PHONE_BOOK_PORT = 7010;
 
 export const ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
